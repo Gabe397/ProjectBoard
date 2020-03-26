88743dd0fc554825d171e79786cd5f942974f4a9
gabe3 <Dinya7991>
2020-03-26 14:01:32 -0400

    Created taskboard Component

---
+++ pboard/src/TaskBoard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './TaskBoardStyles.css';
+
+const TaskBoard =  (props) => {
+    return(
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Taskboard</a>
+            </nav>
+
+            <hr></hr>
+                <div class="container">
+                    <h1>Task Board</h1>
+                    <h1>Task Board</h1>
+                    <h1>Task Board</h1>
+                    <h1>Task Board</h1>
+                </div>
+            <hr></hr>
+
+        </div>
+
+    );
+
+};
+
+
+
+
+
+
+export default TaskBoard;
